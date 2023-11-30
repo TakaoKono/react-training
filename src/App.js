db6123ee8aef0ae6a7e41b3d952d4a75921dfd70
TakaoKono <takao.kouno@datamix.co.jp>
2023-11-30 21:31:19 +0900

    slide

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
-const App = () => {
-  return (
-    <div>
-      <h1>テスト</h1>
-    </div>
-  );
-}
-
-export default App;
+import { View, WebView } from 'react-native';  
+  
+const GoogleSlidesViewer = () => {  
+  const url = 'https://docs.google.com/presentation/d/1-0BpAyuVaGfn5Ob5BwtAvEjg9zIGyTSTmhbo3s0G_GE/edit?usp=drive_link/embed?rm=minimal&hideBorder=true';  
+  
+  return (  
+    <View style={{ flex: 1 }}>  
+      <WebView source={{ uri: url }} />  
+    </View>  
+  );  
+};  
+  
+export default GoogleSlidesViewer;  
